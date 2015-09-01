06e186e3f9dcd598687662dffc4877a23bd1c21a
mbilker <me@mbilker.us>
2015-09-01 16:25:20 -0400

    Linting the font directory

--- modules/font/manifests/base.pp
+++ modules/font/manifests/base.pp
@@ -1,20 +1,27 @@
+# Manages font related files
+
 class font::base {
-	include desktop::xorg
-	package {'fontconfig':
-		require => Package['xorg-server']
-	}
-	file {'/usr/share/fonts':
-		ensure => directory,
-		mode => '0644',
-		require => Package['fontconfig']
-	}
-	file {'/usr/share/fonts/TTF':
-		ensure => directory,
-		mode => '0644',
-		require => Package['fontconfig']
-	}
-	exec {'Update font cache (fc-cache)':
-		command => shellquote('/usr/bin/env', 'fc-cache', '-f'),
-		refreshonly => true
-	}
+  include desktop::xorg
+
+  package { 'fontconfig':
+    ensure  => present,
+    name    => 'fontconfig-infinality-ultimate',
+    require => Package['xorg-server']
+  }
+
+  file { '/usr/share/fonts':
+    ensure  => directory,
+    mode    => '0644',
+    require => Package['fontconfig']
+  }
+  file { '/usr/share/fonts/TTF':
+    ensure  => directory,
+    mode    => '0644',
+    require => Package['fontconfig']
+  }
+
+  exec { 'Update font cache (fc-cache)':
+    command     => shellquote('/usr/bin/env', 'fc-cache', '-f'),
+    refreshonly => true
+  }
 }
