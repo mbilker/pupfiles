120a0baf0bb28d5bd571970087fbe6e194681fd5
mbilker <me@mbilker.us>
2015-09-01 16:33:16 -0400

    Linting the enduser_file directory

--- modules/enduser_file/manifests/ini.pp
+++ modules/enduser_file/manifests/ini.pp
@@ -1,30 +1,33 @@
+# Defines user ini file setting
+
 define enduser_file::ini (
-	$filename,
-	$section = '',
-	$setting,
-	$value,
-	$key_val_separator = '=',
-	$mode = '0600'
+  $filename,
+  $setting,
+  $value,
+  $section = '',
+  $key_val_separator = '=',
+  $mode = '0600'
 ) {
-	if ! defined(Enduser_file[$filename]) {
-		enduser_file {$filename:
-			mode => $mode
-		}
-	}
-	ini_setting {"/home/mbilker/$filename/$section/$setting":
-		path => "/home/mbilker/$filename",
-		section => $section,
-		setting => $setting,
-		value => $value,
-		key_val_separator => $key_val_separator,
-		require => Enduser_file[$filename]
-	}
-	ini_setting {"/root/$filename/$section/$setting":
-		path => "/root/$filename",
-		section => $section,
-		setting => $setting,
-		value => $value,
-		key_val_separator => $key_val_separator,
-		require => Enduser_file[$filename]
-	}
+  if ! defined(Enduser_file[$filename]) {
+    enduser_file {$filename:
+      mode => $mode
+    }
+  }
+
+  ini_setting { "/home/mbilker/${filename}/${section}/${setting}":
+    path              => "/home/mbilker/${filename}",
+    section           => $section,
+    setting           => $setting,
+    value             => $value,
+    key_val_separator => $key_val_separator,
+    require           => Enduser_file[$filename]
+  }
+  ini_setting { "/root/${filename}/${section}/${setting}":
+    path              => "/root/${filename}",
+    section           => $section,
+    setting           => $setting,
+    value             => $value,
+    key_val_separator => $key_val_separator,
+    require           => Enduser_file[$filename]
+  }
 }
--- modules/enduser_file/manifests/init.pp
+++ modules/enduser_file/manifests/init.pp
@@ -1,43 +1,46 @@
+# Defines enduser_file resource type to add a file to my user and root
+
 define enduser_file (
-	$filename = $name,
-	$ensure = present,
-	$mode = '0600',
-	$source = false,
-	$recurse = false,
-	$content = false,
-	$replace = true,
-	$noroot = false,
-	$target = false,
-	$absolutetarget = true
+  $filename = $name,
+  $ensure = present,
+  $mode = '0600',
+  $source = false,
+  $recurse = false,
+  $content = false,
+  $replace = true,
+  $noroot = false,
+  $target = false,
+  $absolutetarget = true
 ) {
-	enduser_file::single {"/home/mbilker/$name":
-		filename => $filename,
-		owner => 'mbilker',
-		group => 'users',
-		ensure => $ensure,
-		mode => $mode,
-		source => $source,
-		recurse => $recurse,
-		content => $content,
-		replace => $replace,
-		target => $target,
-		absolutetarget => $absolutetarget,
-		targetprefix => '/home/mbilker'
-	}
-	if ! $noroot {
-		enduser_file::single {"/root/$name":
-			filename => $filename,
-			owner => 'root',
-			group => 'root',
-			ensure => $ensure,
-			mode => $mode,
-			source => $source,
-			recurse => $recurse,
-			content => $content,
-			replace => $replace,
-			target => $target,
-			absolutetarget => $absolutetarget,
-			targetprefix => '/root'
-		}
-	}
+  enduser_file::single { "/home/mbilker/${name}":
+    ensure         => $ensure,
+    filename       => $filename,
+    owner          => 'mbilker',
+    group          => 'users',
+    mode           => $mode,
+    source         => $source,
+    recurse        => $recurse,
+    content        => $content,
+    replace        => $replace,
+    target         => $target,
+    absolutetarget => $absolutetarget,
+    targetprefix   => '/home/mbilker'
+  }
+
+  if ! $noroot {
+    enduser_file::single { "/root/${name}":
+      ensure         => $ensure,
+      filename       => $filename,
+      owner          => 'root',
+      group          => 'root',
+      mode           => $mode,
+      source         => $source,
+      recurse        => $recurse,
+      content        => $content,
+      replace        => $replace,
+      target         => $target,
+      absolutetarget => $absolutetarget,
+      targetprefix   => '/root'
+    }
+  }
 }
