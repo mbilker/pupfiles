cdbca181ed250ac9fe08246c86d87e366384fe41
mbilker <me@mbilker.us>
2015-09-02 22:12:19 -0400

    Better handling with yaourt because of no --asroot

--- modules/aur_package/manifests/init.pp
+++ modules/aur_package/manifests/init.pp
@@ -1,12 +1,34 @@
 # Installs a Arch Linux AUR package
 
-define aur_package (
-  $ensure = installed
+define aur_package(
+  $ensure = 'present',
 ) {
-  require aur_package::yaourt
+  include aur_package::yaourt
 
-  package { $title:
-    ensure => $ensure,
-    name   => $name
+  case $ensure {
+    'present': {
+      exec { "aur_package::install::${name}":
+        require     => Class[aur_package::yaourt],
+        cwd         => '/tmp',
+        environment => 'HOME=/home/mbilker',
+        command     => "/usr/bin/yaourt -S --noconfirm ${name}",
+        unless      => "/usr/bin/yaourt -Qk ${name}",
+        user        => 'mbilker',
+        logoutput   => on_failure,
+        timeout     => 1800,
+      }
+    }
+    'absent': {
+      exec { "aur_package::remove::${name}":
+        require   => Class[aur_package::yaourt],
+        command   => "/usr/bin/yaourt -Rs ${name}",
+        onlyif    => "/usr/bin/yaourt -Qi ${name}",
+        user      => 'mbilker',
+        logoutput => on_failure,
+      }
+    }
+    default: {
+      fail("Aur_package[${name}] ensure parameter must be either 'present' or 'absent'")
+    }
   }
 }
