4558ea7c184ce4b7f23fd514980cdf3ad3a2e4ed
mbilker <me@mbilker.us>
2015-10-11 16:40:23 -0400

    Update packages required by Intel graphics

--- modules/desktop/manifests/graphics/intel.pp
+++ modules/desktop/manifests/graphics/intel.pp
@@ -1,33 +1,44 @@
+# Set up Intel drivers for graphical environment
+
 class desktop::graphics::intel {
   include base::mkinitcpio
   include base::packaging::multilib
   include desktop::xorg
 
-  package {'xf86-video-intel':}
-  package {'libva-intel-driver':}
-  package {'driconf':}
+  package { ['xf86-video-intel', 'libva-intel-driver', 'driconf']:
+    ensure => present
+  }
+  package { ['mesa', 'mesa-libgl']:
+    ensure => present
+  }
+
+  if $::os['architecture'] == 'x86_64' {
+    package { ['lib32-mesa', 'lib32-mesa-libgl']:
+      ensure => present
+    }
+  }
 
   $i915_command = shellquote(
     '/usr/bin/env', 'echo',
     'MODULES="$MODULES i915"'
   )
 
-  exec {'/etc/mkinitcpio.conf/modules/i915':
-    command => "$i915_command >> /etc/mkinitcpio.conf",
-		unless => shellquote(
-			'/usr/bin/env', 'grep', '-P',
-			'^MODULES="\$MODULES i915"$',
-			'/etc/mkinitcpio.conf'
-		),
-		notify => Exec['mkinitcpio']
-	}
+  exec { '/etc/mkinitcpio.conf/modules/i915':
+    command => "${i915_command} >> /etc/mkinitcpio.conf",
+    unless  => shellquote(
+      '/usr/bin/env', 'grep', '-P',
+      '^MODULES="\$MODULES i915"$',
+      '/etc/mkinitcpio.conf'
+    ),
+    notify  => Exec['mkinitcpio']
+  }
 
-	file {'/etc/X11/xorg.conf.d/20-intel.conf':
-		source => 'puppet:///modules/desktop/graphics/intel/20-intel.conf',
-		require => Class['desktop::xorg']
-	}
+  file { '/etc/X11/xorg.conf.d/20-intel.conf':
+    source  => 'puppet:///modules/desktop/graphics/intel/20-intel.conf',
+    require => Class['desktop::xorg']
+  }
 
-	enduser_file {'.drirc':
-		source => 'desktop/graphics/intel'
-	}
+  enduser_file { '.drirc':
+    source => 'desktop/graphics/intel'
+  }
 }
