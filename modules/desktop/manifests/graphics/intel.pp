b53e0e313900c6a368e233b9f340795212cd4856
mbilker <me@mbilker.us>
2015-10-11 14:35:19 -0400

    Remove intel-dri package, missing from repos

--- modules/desktop/manifests/graphics/intel.pp
+++ modules/desktop/manifests/graphics/intel.pp
@@ -1,17 +1,17 @@
 class desktop::graphics::intel {
+	include base::mkinitcpio
+	include base::packaging::multilib
+	include desktop::xorg
+
 	package {'xf86-video-intel':}
 	package {'libva-intel-driver':}
-	package {'intel-dri':}
 	package {'driconf':}
-	include base::packaging::multilib
-	package {'lib32-intel-dri':
-		require => Class['base::packaging::multilib']
-	}
-	include base::mkinitcpio
+
 	$i915_command = shellquote(
 		'/usr/bin/env', 'echo',
 		'MODULES="$MODULES i915"'
 	)
+
 	exec {'/etc/mkinitcpio.conf/modules/i915':
 		command => "$i915_command >> /etc/mkinitcpio.conf",
 		unless => shellquote(
@@ -21,11 +21,12 @@ class desktop::graphics::intel {
 		),
 		notify => Exec['mkinitcpio']
 	}
-	include desktop::xorg
+
 	file {'/etc/X11/xorg.conf.d/20-intel.conf':
 		source => 'puppet:///modules/desktop/graphics/intel/20-intel.conf',
 		require => Class['desktop::xorg']
 	}
+
 	enduser_file {'.drirc':
 		source => 'desktop/graphics/intel'
 	}
