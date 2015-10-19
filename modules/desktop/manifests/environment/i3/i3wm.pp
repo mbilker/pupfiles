de6d36c59969d579b37196bb00d579f1961587a2
mbilker <me@mbilker.us>
2015-10-19 01:15:24 -0400

    Create .i3 directory in mbilker home folder

--- modules/desktop/manifests/environment/i3/i3wm.pp
+++ modules/desktop/manifests/environment/i3/i3wm.pp
@@ -1,5 +1,9 @@
 class desktop::environment::i3::i3wm {
-	enduser_file {'.i3/config':
-		source => 'desktop/i3'
-	}
+  enduser_file { '.i3':
+    ensure => directory
+  }
+
+  enduser_file { '.i3/config':
+    source => 'desktop/i3'
+  }
 }
