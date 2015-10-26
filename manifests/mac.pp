08a43d77bfd02583607141d27bb41b6427ec22eb
mbilker <me@mbilker.us>
2015-10-25 23:10:35 -0400

    Add mac deployment

---
+++ manifests/mac.pp
@@ -0,0 +1,16 @@
+class { 'base::mbilker': }
+class { 'console': }
+
+class { 'pacman':
+  enable_aur       => true,
+  yaourt_exec_user => 'mbilker',
+  yaourt_exec_home => '/home/mbilker'
+}
+
+class { 'desktop':
+  browser          => 'chrome_dev',
+  dpi              => 96,
+  games            => false,
+  graphics_drivers => 'intel',
+  laptop           => true
+}
