ebc6a075d111f31187f6841535eeb370a5082539
mbilker <me@mbilker.us>
2015-11-03 18:25:34 -0500

    Update intel graphics configuration

--- modules/desktop/manifests/graphics/intel.pp
+++ modules/desktop/manifests/graphics/intel.pp
@@ -18,27 +18,8 @@ class desktop::graphics::intel {
     }
   }
 
-  $i915_command = shellquote(
-    '/usr/bin/env', 'echo',
-    'MODULES="$MODULES i915"'
-  )
-
-  exec { '/etc/mkinitcpio.conf/modules/i915':
-    command => "${i915_command} >> /etc/mkinitcpio.conf",
-    unless  => shellquote(
-      '/usr/bin/env', 'grep', '-P',
-      '^MODULES="\$MODULES i915"$',
-      '/etc/mkinitcpio.conf'
-    ),
-    notify  => Exec['mkinitcpio']
-  }
-
   file { '/etc/X11/xorg.conf.d/20-intel.conf':
     source  => 'puppet:///modules/desktop/graphics/intel/20-intel.conf',
     require => Class['desktop::xorg']
-  }
-
-  enduser_file { '.drirc':
-    source => 'desktop/graphics/intel'
   }
 }
